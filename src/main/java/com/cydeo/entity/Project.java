4620c8f211375ba69ac233f7c1614a24d3acad2f
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-11-06 13:06:00 -0500

    touching The Task

--- src/main/java/com/cydeo/entity/Project.java
+++ src/main/java/com/cydeo/entity/Project.java
@@ -18,7 +18,7 @@ import java.time.LocalDate;
 @Where(clause = "is_deleted=false")
 public class Project extends BaseEntity {
 
-
+    @Column(unique = true)
     private String projectCode;
     private String projectName;
 
@@ -33,7 +33,7 @@ public class Project extends BaseEntity {
     private String projectDetail;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="manager_id")
+    @JoinColumn(name = "manager_id")
     private User assignedManager;
 
 
